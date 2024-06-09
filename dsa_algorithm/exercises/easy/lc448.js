66f3916e7f16081706a1497c733d80be90092ebd
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-06-09 09:33:12 -0300

    feat: lc448.js

---
+++ dsa_algorithm/exercises/easy/lc448.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+// 448. Find All Numbers Disappeared in an Array
+// So
+
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 105
+// 1 <= nums[i] <= n
+ 
+
+var findDisappearedNumbers = function(nums) {
+    let obj={}
+    for(let n of nums){
+        obj[n]=1
+    }
+    let arr=[]
+    for(let i=1;i<=nums.length;i++){
+        if(!obj[i]) arr.push(i)
+    }
+    return arr
+};
