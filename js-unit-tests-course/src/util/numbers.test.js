a75950237e834613d63ff46639a6e73a48ea57f2
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-09-22 13:19:48 -0300

    integration tests

--- js-unit-tests-course/src/util/numbers.test.js
+++ js-unit-tests-course/src/util/numbers.test.js
@@ -1,6 +1,6 @@
-import { it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
-import { transformToNumber } from './numbers';
+import { cleanNumbers, transformToNumber } from './numbers';
 
 it('should transform a string number to a number of type number', () => {
   const input = '1';
@@ -28,3 +28,21 @@ it('should yield NaN for non-transformable values', () => {
   expect(result).toBeNaN();
   expect(result2).toBeNaN();
 });
+
+describe('cleanNumbers()', () => {
+  it('should return an array of number values if an array of string number values is provided', () => {
+    const numbersValues = ['1', '3', '5']
+
+    const cleanedNumbers = cleanNumbers(numbersValues)
+
+    expect(cleanedNumbers[0]).toBeTypeOf('number');
+  })
+
+  it('should throw an error if an array with at leaast one empty string is provided', () => {
+    const numberValues = ['', 1];
+
+    const cleanFn = () => cleanNumbers(numberValues);
+
+    expect(cleanFn).toThrow();
+  })
+})
